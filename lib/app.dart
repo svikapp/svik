491fbb2427c37f8d86357c1600f63ac5e252d4be
Praveen <itspraveen2004@gmail.com>
2023-02-26 18:39:33 +0530

    app.dart changes

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:svik2/presentation/pages/homepage_view.dart';
+import 'package:svik2/presentation/pages/login_view.dart';
+import 'package:svik2/presentation/pages/signup_view.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
